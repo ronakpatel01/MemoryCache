83bf3e2b259b96d1afae77ad05e9eff72ae652fd
Ronak Patel <ronak.patel@dolphin-developers.com>
2022-10-22 14:55:35 +0100

    Added Unit Tests

---
+++ MemoryCacheTest/MemoryCacheTest.cs
@@ -0,0 +1,195 @@
+﻿using Finbourne.MemoryCache;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MemoryCacheTest
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DOB { get; set; }
+    }
+
+
+
+    [TestClass]
+    public class MemoryCacheTests 
+    {
+        [TestMethod]
+        public void TestAddToCache()
+        {
+            IMemoryCache mc = MemoryCache.Instance(5);
+
+            mc.Add("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+
+            Assert.AreEqual(1, mc.GetCount());
+
+            User userFred = mc.Get("Fred") as User;
+            Assert.AreEqual("Fred", userFred.FirstName);
+            Assert.AreEqual("Smith", userFred.Surname);
+            Assert.AreEqual(new DateTime(1995, 01, 15), userFred.DOB);
+        }
+
+        [TestMethod]
+        public void TestAddExistingwithoutReplacing()
+        {
+            IMemoryCache mc = MemoryCache.Instance(5);
+
+            mc.Add("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+            mc.Add("Fred", new User { FirstName = "James", Surname = "Layley", DOB = new DateTime(1990, 08, 03) });
+
+            Assert.AreEqual(1, mc.GetCount());
+
+            User userFred = mc.Get("Fred") as User;
+            Assert.AreEqual("Fred", userFred.FirstName);
+            Assert.AreEqual("Smith", userFred.Surname);
+            Assert.AreEqual(new DateTime(1995, 01, 15), userFred.DOB);
+        }
+
+        [TestMethod]
+        public void TestAddExistingwithReplacing()
+        {
+            IMemoryCache mc = MemoryCache.Instance(5);
+
+            mc.AddOrReplace("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+            mc.AddOrReplace("Fred", new User { FirstName = "James", Surname = "Layley", DOB = new DateTime(1990, 08, 03) });
+
+            Assert.AreEqual(1, mc.GetCount());
+
+            User userFred = mc.Get("Fred") as User;
+            Assert.AreEqual("James", userFred.FirstName);
+            Assert.AreEqual("Layley", userFred.Surname);
+            Assert.AreEqual(new DateTime(1990, 08, 03), userFred.DOB);
+        }
+
+        [TestMethod]
+        public void TestMultipleAdds()
+        {
+            IMemoryCache mc = MemoryCache.Instance(5);
+
+            mc.Add("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+            mc.Add("James", new User { FirstName = "James", Surname = "Layley", DOB = new DateTime(1990, 08, 03) });
+            mc.Add("Mike", new User { FirstName = "Mike", Surname = "Bloggs", DOB = new DateTime(1963, 09, 17) });
+
+            Assert.AreEqual(3, mc.GetCount());
+
+            User userFred = mc.Get("Fred") as User;
+            Assert.AreEqual("Fred", userFred.FirstName);
+            Assert.AreEqual("Smith", userFred.Surname);
+            Assert.AreEqual(new DateTime(1995, 01, 15), userFred.DOB);
+
+            User userJames = mc.Get("James") as User;
+            Assert.AreEqual("James", userJames.FirstName);
+            Assert.AreEqual("Layley", userJames.Surname);
+            Assert.AreEqual(new DateTime(1990, 08, 03), userJames.DOB);
+
+            User userMike = mc.Get("Mike") as User;
+            Assert.AreEqual("Mike", userMike.FirstName);
+            Assert.AreEqual("Bloggs", userMike.Surname);
+            Assert.AreEqual(new DateTime(1963, 09, 17), userMike.DOB);
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            IMemoryCache mc = MemoryCache.Instance(5);
+
+            mc.Add("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+            mc.Add("James", new User { FirstName = "James", Surname = "Layley", DOB = new DateTime(1990, 08, 03) });
+            mc.Add("Mike", new User { FirstName = "Mike", Surname = "Bloggs", DOB = new DateTime(1963, 09, 17) });
+
+            Assert.IsTrue(mc.Contains("Fred"));
+            Assert.IsTrue(mc.Contains("James"));
+            Assert.IsTrue(mc.Contains("Mike"));
+            Assert.IsFalse(mc.Contains("Paul"));
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            IMemoryCache mc = MemoryCache.Instance(5);
+
+            mc.Add("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+            mc.Add("James", new User { FirstName = "James", Surname = "Layley", DOB = new DateTime(1990, 08, 03) });
+            mc.Add("Mike", new User { FirstName = "Mike", Surname = "Bloggs", DOB = new DateTime(1963, 09, 17) });
+
+            mc.Remove("James");
+
+            Assert.IsTrue(mc.Contains("Fred"));
+            Assert.IsFalse(mc.Contains("James"));
+            Assert.IsTrue(mc.Contains("Mike"));
+        }
+
+        [TestMethod]
+        public void TestOverflow()
+        {
+            IMemoryCache mc = MemoryCache.Instance(2);
+
+            mc.Add("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+            mc.Add("James", new User { FirstName = "James", Surname = "Layley", DOB = new DateTime(1990, 08, 03) });
+            mc.Add("Mike", new User { FirstName = "Mike", Surname = "Bloggs", DOB = new DateTime(1963, 09, 17) });
+
+            Assert.IsFalse(mc.Contains("Fred"));
+            Assert.IsTrue(mc.Contains("James"));
+            Assert.IsTrue(mc.Contains("Mike"));
+        }
+
+        [TestMethod]
+        public void TestOverflowAfterRead()
+        {
+            IMemoryCache mc = MemoryCache.Instance(2);
+
+            mc.Add("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+            mc.Add("James", new User { FirstName = "James", Surname = "Layley", DOB = new DateTime(1990, 08, 03) });
+            User userFred = mc.Get("Fred") as User;
+            mc.Add("Mike", new User { FirstName = "Mike", Surname = "Bloggs", DOB = new DateTime(1963, 09, 17) });
+
+            Assert.IsTrue(mc.Contains("Fred"));
+            Assert.IsFalse(mc.Contains("James"));
+            Assert.IsTrue(mc.Contains("Mike"));
+        }
+
+        [TestMethod]
+        public void TestGetAll()
+        {
+            IMemoryCache mc = MemoryCache.Instance(5);
+
+            mc.Add("Fred", new User { FirstName = "Fred", Surname = "Smith", DOB = new DateTime(1995, 01, 15) });
+            mc.Add("James", new User { FirstName = "James", Surname = "Layley", DOB = new DateTime(1990, 08, 03) });
+            mc.Add("Mike", new User { FirstName = "Mike", Surname = "Bloggs", DOB = new DateTime(1963, 09, 17) });
+
+            Assert.IsTrue(mc.Contains("Fred"));
+            Assert.IsTrue(mc.Contains("James"));
+            Assert.IsTrue(mc.Contains("Mike"));
+
+            foreach (var cachedObject in mc)
+            {
+                switch (cachedObject.Key)
+                {
+                    case "Fred":
+                        User userFred = cachedObject.Value as User;
+                        Assert.AreEqual("Fred", userFred.FirstName);
+                        Assert.AreEqual("Smith", userFred.Surname);
+                        Assert.AreEqual(new DateTime(1995, 01, 15), userFred.DOB);
+                        break;
+                    case "James":
+                        User userJames = cachedObject.Value as User;
+                        Assert.AreEqual("James", userJames.FirstName);
+                        Assert.AreEqual("Layley", userJames.Surname);
+                        Assert.AreEqual(new DateTime(1990, 08, 03), userJames.DOB);
+                        break;
+                    case "Mike":
+                        User userMike = cachedObject.Value as User;
+                        Assert.AreEqual("Mike", userMike.FirstName);
+                        Assert.AreEqual("Bloggs", userMike.Surname);
+                        Assert.AreEqual(new DateTime(1963, 09, 17), userMike.DOB);
+                        break;
+                    default:
+                        Assert.Fail("User should not exist");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ MemoryCacheTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MemoryCacheTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MemoryCacheTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4f5f567-bd46-4504-9969-28859ef1e0af")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
